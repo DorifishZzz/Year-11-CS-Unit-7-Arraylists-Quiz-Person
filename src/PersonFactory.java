2634276bd910b9366650d9560023ff3207f399c3
dorazhao <doraz2025@student.cis.edu.hk>
2023-03-06 13:02:19 +0800

    added person

--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -9,14 +9,44 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
+        allPersons.add(person);
+        for(int i = allPersons.size()-1; i >= 0; i --){
+                String w = allPersons.get(i).getLastName();
+                String q = allPersons.get(i-1).getLastName();
+
+            if(w.compareTo(q) == 1){
+                allPersons.set(i, allPersons.get(i-1));
+            }
+            if(w.compareTo(q) == -1){
+                continue;
+            }
+            if(w.compareTo(q) == 0) {
+                continue;
+            }
+
+        }
 
     }
 
     public ArrayList<Person> under18() {
+        ArrayList<Person> under18Only = new ArrayList<>();
+        for(int i = 0; i < allPersons.size(); i++){
+            if (allPersons.get(i).getAge() == 18){
+                under18Only.add(allPersons.get(i));
+            }
+        }
+        return under18Only;
 
     }
 
     public boolean checkLastName(String lastName) {
-
+        for(int i = 0; i < allPersons.size(); i++ ){
+            String last = allPersons.get(i).getLastName();
+            if (last.equals(lastName)){
+                return true;
+            }
+            }
+        return false;
+        }
     }
 }
